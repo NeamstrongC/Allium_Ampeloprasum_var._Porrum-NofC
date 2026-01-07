517a41dc94886b23fbb8dddaf43f4e6525b41b92
N ASTRONAUT <137132823+NeamstrongC@users.noreply.github.com>
2026-01-07 17:32:49 -0500

    Delete assets/js/script.js

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-const button = document.getElementById('butonad')
-
-button.addEventListener('click', doSomething)
-
-function doSomething(){
-    alert("EN ESTE MOMENTO ESTAMOS TRABAJANDO PARA TENER DISPONIBLE Y A SU SERVICIO ESTA SECCIÓN, DIOS NOS BENDIGA. N DE COLOMBIA.")
-}
