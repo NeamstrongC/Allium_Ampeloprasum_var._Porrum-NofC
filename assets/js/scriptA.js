fbfb23b13c259bcf512de7cf4928dd102fa064c9
N ASTRONAUT <137132823+NeamstrongC@users.noreply.github.com>
2026-01-09 22:42:24 -0500

    Enhance Swal.fire options in scriptA.js
    Added footer, backdrop, and image options to Swal.fire.

--- assets/js/scriptA.js
+++ assets/js/scriptA.js
@@ -10,9 +10,27 @@ document.addEventListener('DOMContentLoaded', function () {
             Swal.fire({
                 title: '¡N SERVERS!',
                 text: 'En este momento estamos trabajando para tener lista está y otras secciones N, junto con los terminos y condiciones + las politicas de privacidad.',
-                icon: 'success',
+                // icon:'warning',
                 confirmButtonText: 'Entendido',
                 // Puedes agregar más opciones aquí
+                footer: 'N DE COLOMBIA.',
+                // background:'',
+                backdrop:'true',
+                // timer:'',
+                // toast:'',
+                // position:'',
+                allowOutsideClick:false,
+                stopKeydownPropagation:false,
+                allowEscapeKey:false,
+                allowEnterKey:false,
+                // customClass:{},
+                imageUrl:'assets/images/cookieN.png',
+                imageWidth:'40%',
+
+
+
+
+
             });
         });
     }
